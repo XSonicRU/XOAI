656d0a097ea0c6c2ced61e735d2b1ef546888047
Gohan <miva-2002@mail.ru>
2018-04-21 08:29:25 +0300

    Cleaned some unused junk, code is now fully commented

--- src/Ctrl.java
+++ src/Ctrl.java
@@ -1,62 +1,51 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.util.Random;
-import java.util.Vector;
 
-class Ctrl {//Class used for recognition of the field
+class Ctrl {//Class used for recognition of the field and other stuff
     final private int[] coordxx = new int[]{827, 953, 1075, 828, 951, 1076, 827, 947, 1074}; // X screen coordinates
     final private int[] coordyx = new int[]{250, 250, 250, 371, 371, 371, 498, 498, 498};
     final private int[] coordxo = new int[]{827, 950, 1075, 828, 951, 1076, 827, 947, 1074}; //O screen coordinates
     final private int[] coordyo = new int[]{215, 215, 215, 339, 339, 339, 462, 462, 462};
-    private Robot r;
+    private Robot r; //Robot for recognition stuff
 
-    Ctrl(int swait, int swork) throws AWTException, InterruptedException {
-        r = new Robot();
-        Random ra = new Random();
-        Vector<Integer> v;
-        Thread.sleep(swait * 1000);
-        Timer t = new Timer();
-        t.start();
-        while (t.time < swork) {
-            Thread.sleep(5);
-            presspos(ra.nextInt(9));
+    Ctrl(int swait, int swork) throws AWTException, InterruptedException { //Constructor that makes all the work
+        r = new Robot(); //Initialization of Robot
+        Random ra = new Random(); //Random initialization
+        Thread.sleep(swait * 1000); //Wait swait seconds before start
+        Timer t = new Timer(); //Timer initialization for time count
+        t.start(); //Starting timer
+        while (t.time < swork) { //While time isnt over
+            Thread.sleep(5); //Some slowdown to avoid lags
+            presspos(ra.nextInt(9)); //press in random cell of the field
         }
-        t.interrupt();
+        t.interrupt(); //stop the timer for programm to finish
 
     }
 
-    private Vector<Integer> getFreeSpaces() {
-        Vector<Integer> res = new Vector<>();
-        for (int i = 0; i < 9; i++) {
-            if (getpos(i) == 0) {
-                res.add(i);
+    private void presspos(int pos) { //Method for pressing cells
+        r.mouseMove(coordxx[pos], coordyx[pos]); //move mouse to a cell
+        r.mousePress(InputEvent.getMaskForButton(1)); //Press a cell
+        r.mouseRelease(InputEvent.getMaskForButton(1)); //Release a mouse for the next press
+    }
+
+    private int getpos(int pos) { //Method for getting state of pos cell
+        Color c = r.getPixelColor(coordxx[pos], coordyx[pos]); //Take middle color of pos cell
+        Color c1 = r.getPixelColor(coordxo[pos], coordyo[pos]); //Take upper color of pos cell
+        //Upper color is for 0 recognition, without it its impossible to detect whether its a 0 or X or its empty
+        if (c.getRed() == 0 && c.getGreen() == 0 && c.getBlue() == 0) { // if middle of cell is black
+            if (c1.getRed() == 255 && c1.getGreen() == 255 && c1.getBlue() == 255) { //if upper of cell is white
+                return 2; //2 is for 0 cell
+            } else { //if upper of cell is dark
+                return 0; //0 is for empty cell
             }
-        }
-        return res;
-    }
-
-    private void presspos(int pos) {
-        r.mouseMove(coordxx[pos], coordyx[pos]);
-        r.mousePress(InputEvent.getMaskForButton(1));
-        r.mouseRelease(InputEvent.getMaskForButton(1));
-    }
-
-    private int getpos(int pos) {
-        Color c = r.getPixelColor(coordxx[pos], coordyx[pos]);
-        Color c1 = r.getPixelColor(coordxo[pos], coordyo[pos]);
-        if (c.getRed() == 0 && c.getGreen() == 0 && c.getBlue() == 0) {
-            if (c1.getRed() == 255 && c1.getGreen() == 255 && c1.getBlue() == 255) {
-                return 2;
-            } else {
-                return 0;
-            }
-        } else {
-            return 1;
+        } else { //if middle of cell is white
+            return 1; //1 is for X cell
         }
     }
 }
 
-class Timer extends Thread {
+class Timer extends Thread { //Timer in another thread for time counting
     @Override
     public void run() {
         try {
